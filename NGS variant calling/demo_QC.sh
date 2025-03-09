7f2ff4370c1e6ed63d8adec5a8eec1aeab7691ab
lingo1stt <134258750+lingo1stt@users.noreply.github.com>
2025-03-09 16:57:06 +0800

    Update demo_QC.sh

--- NGS variant calling/demo_QC.sh
+++ NGS variant calling/demo_QC.sh
@@ -1,47 +1,40 @@
 #### Quality control ############
-##write a for loop to conduct fastQC and trimmomatic at the same time
+##write a for loop to conduct fastQC 
+#!/bin/bash
 run_fastqc() {
   if [ -z "$1" ]; then
         echo "please provide file name。"
         return 1
   fi
-  ###執行
   echo "run FastQC: $1"
-    for file in L*.fq.gz
-    do
-	    if [ -e "$file"]; then
-		    fastqc "$file"
-	    else 
-		    echo "NO files existed"
-	    fi
+  ###如果沒有符合的檔案則變成空字串
+  shopt -s nullglob
+  files = ($1)
+  if [ ${#files[@]} -eq 0 ]; then
+  	echo "no file existed"
+   	return 1
+   fi
+   ##執行fastqc
+    for file in "${files[@]}"; 
+    do  fastqc "$file"
     done
- 
 }
 
-
+###檢查是否有安裝fastqc
 whereis fastqc > check.txt
 if ! grep -q "/" check.txt ; then
   read -p "Do you want to install FastQC？(y/n): " answer
   if [[ $answer == "y" || $answer == "Y" ]]; then
     sudo apt install fastqc
-    echo -e "Enter file name with regular expression (ex:*.fq.gz):"
-    read answer_2
-    run_fastqc "$answer_2"
-	fi
 else
-  echo -e "cannot conduct fastqc, jump to next step \n"
+  echo -e "cannot conduct fastqc"
+  exit 1
+   fi
 fi
-
-
-for file in L*.fq.gz
-do
-	if [ -e "$file"]; then
-		echo "file existed"
-		fastqc "$file"
-	else 
-		echo "NO files existed"
-	fi
-done
+###執行run_fastqc()
+echo "Enter file pattern (e.g., L*.fq.gz):"
+read file_pattern
+run_fastqc "$file_pattern"
 
 ### trimmomatic (sample: L_294 for example)
 trimmomatic PE ~/RILs/01.RawData/L_294/L_294_DKDN220005958-1A_HJTWTDSX3_L2_1.fq.gz ~/RILs/01.RawData/L_294/L_294_DKDN220005958-1A_HJTWTDSX3_L2_2.fq.gz output_forward_paired.fq.gz output_forward_unpaired.fq.gz output_reverse_paired.fq.gz output_reverse_unpaired.fq.gz ILLUMINACLIP:TruSeq3-PE.fa:2:30:10 LEADING:30 TRAILING:30 MINLEN:70
